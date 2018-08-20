7cb9492c3b87d191882cc472727aa68d9cdc523f
Raghavan R <raghavan.r@capgemini.com>
2018-08-20 13:12:38 +1000

    variable type change

--- variable.tf
+++ variable.tf
@@ -1,6 +1,8 @@
 variable "vpc_id" {}
 variable "env" {}
 variable "create_vpc" {}
-variable "route_table_id" {}
+variable "route_table_id" {
+    type = "list"
+}
 variable "destination_cidr_block" {}
 variable "igw_route" {}
